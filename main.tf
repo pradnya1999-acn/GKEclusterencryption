ab21e375f3007caa15c564e5b59529eec7e367d4
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-03-19 16:05:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ variable "sa_key" {
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = "us-central1"
-  remove_default_node_pool = true
+  enable_autopilot   = true
   initial_node_count       = 1
 
-# database_encryption {
-#   state = "ENCRYPTED"
-#   key_name = "projects/parabolic-base-409505/locations/us-central1/keyRings/secret-keyring/cryptoKeys/secret-key"
-#}
+ database_encryption {
+   state = "ENCRYPTED"
+   key_name = "projects/parabolic-base-409505/locations/us-central1/keyRings/secret-keyring/cryptoKeys/secret-key"
+}
 }
